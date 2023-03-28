17e5cc3674a5efc9c83e3e8238a643e7a1055aeb
Heverson Silva Vasconcelos <heverson.vasconcelos@gmail.com>
2023-03-28 07:28:05 -0400

    Adição do stylesheet

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import {
 } from "@elastic/react-search-ui";
 import { Layout } from "@elastic/react-search-ui-views";
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import "./stylesheet/styles.css";
 
 import {
   buildFacetConfigFromConfig,
